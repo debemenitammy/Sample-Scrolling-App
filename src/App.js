299f9ad2b5ed9c6dd74ac207760a61c9615e6237
Deborah Emeni <deborahemeni@yahoo.com>
2022-06-22 11:27:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Heading from '../src/components/Heading';
+import Loader from '../src/components/Loader';
+import UnsplashImage from '../src/components/UnsplashImage';
+
+import axios from 'axios';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+
+// Style 
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+  }
+`;
+
+const WrapperImage = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
 
 function App() {
+  const [images, setImages] = useState([])
+
+  useEffect(() => {
+    // const apiRoot = "https://api.unsplash.com";
+    const apiRoot = "https://api.pexels.com/v1/";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
+    
+    axios
+      .get(`${apiRoot}/search?query=people`, {
+        headers: {
+          'Authorization': `${accessKey}`
+        }
+      })
+      .then(res => setImages([...images, ...res.data]))
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading />
+      <GlobalStyle />
+      <Loader />
+      <WrapperImage>
+        {images.map(image => (
+          <UnsplashImage url={image.src.large} key={image.id} />
+        ))}
+      </WrapperImage>
     </div>
   );
 }
+
 
 export default App;
